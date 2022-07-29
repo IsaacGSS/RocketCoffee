d7e045c61e13f467237e02c759a71ad7f374c093
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2022-07-29 18:32:30 -0300

    🎉
    Desafio sendo entregue com todas as exigências.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const open = document.querySelector('.open');
+const close = document.querySelector('.close');
+const nav = document.querySelector('.navbar');
+const main = document.querySelector('main');
 
+open.addEventListener('click', function(){
+  open.style.display = 'none'
+  close.style.display = 'block'
+
+  nav.style.display = 'flex';
+  main.style.display = 'none';
+})
+
+close.addEventListener('click', function(){
+  open.style.display = 'block'
+  close.style.display = 'none'
+
+  nav.style.display = 'none';
+  main.style.display = 'flex';
+})
